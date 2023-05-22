41a6942866af1f6eb3f88ae10d3e2f263834042a
DanielZornek <daniel_zornek91@outlook.com>
2023-05-21 22:40:36 -0300

    creating and styling components intro

--- IntroAngular/Course001_ElementsComponents/challenge001/src/Componets/CardNews.js
+++ IntroAngular/Course001_ElementsComponents/challenge001/src/Componets/CardNews.js
@@ -1,3 +1,5 @@
+/*
+
 class CardNews extends HTMLElement{
     
     constructor(){
@@ -5,6 +7,41 @@ class CardNews extends HTMLElement{
 
         const shadow = this.attachShadow({mode: "open"});
         shadow.innerHTML = "<h1>Hello Daniel</h1>";
+    }
+}
+
+customElements.define("card-news", CardNews);
+
+*/
+
+class CardNews extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({ mode:"open"} );
+        shadow.appendChild(this.build());
+        // shadow.appendChild(this.styles());
+    }
+
+    build(){
+
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles(){
+
     }
 }
 
